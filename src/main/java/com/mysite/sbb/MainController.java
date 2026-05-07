7dbd019fe9df0af79491658fa62fff78f62ec0c9
bighead0831 <bighead0831@naver.com>
2026-05-07 12:40:20 +0900

    기존 404, 500에러 해결: @ResponseBody를 통해 문자열 웹에 정상출력

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,19 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  // 보통 웹요청을 처리하는 클래스에 붙인다.
 public class MainController {
     @GetMapping("/")  // HTTP Get요청을 처리. Controller가 있을 때만 GetMapping을 할 수 있다.
-    public void index() {
-        System.out.println("index() 메서드 호출!");
+    @ResponseBody
+    public String index() {
+        return "SBB";
     }
 
     @GetMapping("/hello")
-    public void hello() {
-        System.out.println("hello() 메서드 호출!");
+    @ResponseBody
+    public String hello() {
+        return "안녕하세요.";
     }
 }
