3bf8d5964b74a38cef86ab09fc8d11a37d33232d
bighead0831 <bighead0831@naver.com>
2026-05-11 10:40:08 +0900

    33강 리다이렉션

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  // 보통 웹요청을 처리하는 클래스에 붙인다.
 public class MainController {
-    @GetMapping("/")  // HTTP Get요청을 처리. Controller가 있을 때만 GetMapping을 할 수 있다.
+    @GetMapping("/sbb")  // HTTP Get요청을 처리. Controller가 있을 때만 GetMapping을 할 수 있다.
     @ResponseBody
     public String index() {
         return "SBB";
@@ -16,5 +16,10 @@ public class MainController {
     @ResponseBody
     public String hello() {
         return "안녕하세요.";
+    }
+
+    @GetMapping("/")  // 리다이렉션
+    public String root() {
+        return "redirect:/question/list";
     }
 }
