44974bfa4ef2e7765d2b9bd5b7a9ddcd95120e84
bighead0831 <bighead0831@naver.com>
2026-05-11 10:26:12 +0900

    30강 questionList 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.6"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com"
+group = "com.mysite"
 version = "0.0.1-SNAPSHOT"
-description = "sbb"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
@@ -19,18 +24,16 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-h2console")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testCompileOnly("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testAnnotationProcessor("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,44 +1,22 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Controller// 보통 웹요청을 처리하는 클래스에 붙인다.
+@Controller
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionRepository questionRepository;
 
-    @GetMapping("/question/list")  // HTTP Get요청을 처리. Controller가 있을 때만 GetMapping을 할 수 있다.
-    @ResponseBody
-    public String list() {
-        List<Question> questions = questionRepository.findAll();
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
 
-        String questionList = questions
-                .stream()
-                .map(q -> "<li>%d / %s</li>".formatted(q.getId(), q.getSubject()))
-                .collect(Collectors.joining("\n"));
-
-        return """
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>질문 목록</title>
-                </head>
-                <body>
-                    <h1>질문 목록</h1>
-                    <ul>
-                        %s
-                    </ul>
-                </body>
-                </html>
-                """.formatted(questionList);
+        return "question_list";
     }
-
 }
